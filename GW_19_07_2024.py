83ac4736df197bc2e937e60aa238bed7df0489da
Srishti Harit <86703642+haritsrishti@users.noreply.github.com>
2024-07-19 23:19:57 +0530

    python code by devansh

--- GW_19_07_2024.py
+++ GW_19_07_2024.py
@@ -12,7 +12,28 @@ def calculate_annual_salary(first_name, last_name,age, annual_salary, annual_bon
     print(response)
     return response
 
-calculate_annual_salary("Vishkaha", "Srivastava", 21, 10000, 1500 )
+calculate_annual_salary("Vishakha", "Srivastava", 21, 10000, 1500 )
+
+'''Write a Python function that accepts four arguments: item_name, quantity, price_per_unit,
+and discount_percentage.The function should calculate
+the total price before discount by multiplying the quantity by the price per unit.
+Then, calculate the discount amount by applying the discount percentage to the total price.
+Finally, calculate the final price after subtracting the discount from the total price.
+The function should return a concatenated string containing the item name, quantity, total price'''
+
+
+item_name=input("item name")
+quantity=int(input("quantity"))
+price_per_unit=float(input("price per unit"))
+discount_percentage=float(input("discount percentage"))
+def totalprice(item_name, quantity, price_per_unit, discount_percentage):
+    total_price=quantity*price_per_unit
+    discount_amount=total_price*(discount_percentage/100)
+    final_price=total_price-discount_amount
+    return f"item name: {item_name}, quantity: {quantity}, total price: {final_price}"
+print(totalprice(item_name, quantity, price_per_unit, discount_percentage))
+
+
 
 
 
