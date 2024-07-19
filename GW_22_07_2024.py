446b9c04d33717ae2f89df7fa5670b6fdfa618ac
Srishti Harit <86703642+haritsrishti@users.noreply.github.com>
2024-07-20 00:29:07 +0530

    LISTS

---
+++ GW_22_07_2024.py
@@ -0,0 +1,67 @@
+marks=[90,34,99,100,1,101]
+print(marks)
+
+print(type(marks))
+# We Have To Find The 0 Index Of An Element
+print(marks[2])
+
+# Find The len Of The List Of Elements
+print(len(marks))
+
+# Add A New Element To The List
+marks.append(100000)
+print(marks)
+
+# Remove An Element From The List
+marks.remove(90)
+print(marks)
+
+marks.pop(0)
+print(marks)
+
+marks.reverse()
+print(marks)
+
+marks.sort()
+print(marks)
+
+marks.insert(1,111)
+print(marks)
+
+list=["apple","mango","orange"]
+print(list)
+
+list.copy()
+print(list)
+
+list.append("banana")
+print(list)
+
+list.remove("mango")
+print(list)
+
+list.pop(1)
+print(list)
+
+list.insert(2,"litchi")
+print(list)
+
+#Add Two List
+x=marks+list
+print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
